b793db27bb2515ab98c99273888ac16ad370e189
Artstation2024\Anderson <nathanieljla@gmail.com>
2024-07-29 15:58:42 -0500

    Fixed uData bug.  Namespaces for morphs can now be striped from ASCII FBX files.

--- src/cg3dguru/__init__.py
+++ src/cg3dguru/__init__.py
@@ -1,5 +1,8 @@
-VERSION = (0, 6, 2)
+VERSION = (0, 6, 4)
 
 __author__ = "Nathaniel Albright"
 __email__ = "developer@3dcg.guru"
 __version__ = '.'.join(map(str, VERSION))
+
+#0.6.4 = Add option to strip SubDeformer name spaces from FBX
+#0.6.3 = Fixes bug where the uData utilities didn't update class versions when finding records
