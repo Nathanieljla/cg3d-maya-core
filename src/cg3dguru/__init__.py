53de925415702ca5080a4b78ae255e2450ea2b10
Artstation2024\Anderson <nathanieljla@gmail.com>
2024-09-04 08:46:45 -0500

    WIP - Conversion from ASCII to Binary FBX

--- src/cg3dguru/__init__.py
+++ src/cg3dguru/__init__.py
@@ -1,8 +1,9 @@
-VERSION = (0, 6, 4)
+VERSION = (0, 7, 0)
 
 __author__ = "Nathaniel Albright"
 __email__ = "developer@3dcg.guru"
 __version__ = '.'.join(map(str, VERSION))
 
-#0.6.4 = Add option to strip SubDeformer name spaces from FBX
+#0.7.0 = Option to use convert ascii fbx to binary
+#0.6.4 = Add option to strip SubDeformer namespaces from FBX
 #0.6.3 = Fixes bug where the uData utilities didn't update class versions when finding records
